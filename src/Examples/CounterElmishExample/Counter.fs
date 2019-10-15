f6de6438a3c7ea3b99d357cfe846c1d62dd5dd38
brad phelan <brad@moduleworks.com>
2019-10-15 19:16:57 +0200

    Prove messages via DU's are not required.

--- src/Examples/CounterElmishExample/Counter.fs
+++ src/Examples/CounterElmishExample/Counter.fs
@@ -17,6 +17,30 @@ module ViewExt =
         static member customControl (attrs: TypedAttr<CustomControl> list): View =
             Views.create<CustomControl>(attrs)
 
+// see also 
+// http://stackoverflow.com/questions/4949941/convert-string-to-system-datetime-in-f
+
+module TryParser =
+    // convenient, functional TryParse wrappers returning option<'a>
+    let tryParseWith (tryParseFunc: string -> bool * _) = tryParseFunc >> function
+        | true, v    -> Some v
+        | false, _   -> None
+
+    let parseDate   = tryParseWith System.DateTime.TryParse
+    let parseInt    = tryParseWith System.Int32.TryParse
+    let parseIntWithDefault d = parseInt >> (Option.defaultValue d) 
+    let parseSingle = tryParseWith System.Single.TryParse
+    let parseDouble = tryParseWith System.Double.TryParse
+    // etc.
+
+    // active patterns for try-parsing strings
+    let (|Date|_|)   = parseDate
+    let (|Int|_|)    = parseInt
+    let (|Single|_|) = parseSingle
+    let (|Double|_|) = parseDouble
+
+open TryParser
+
 module Counter =
 
     type CounterState = {
@@ -31,30 +55,53 @@ module Counter =
     | Increment
     | Decrement
 
-    let update (msg: Msg) (state: CounterState) : CounterState =
-        match msg with
-        | Increment -> { state with count =  state.count + 1 }
-        | Decrement -> { state with count =  state.count - 1 }
-    
+    let update msg (state: CounterState) : CounterState =
+        msg state
+
+    let increment  (state: CounterState) : CounterState = { state with count =  state.count + 1 }
+    let decrement  (state: CounterState) : CounterState = { state with count =  state.count + 1 }
+    let set_count  (state:CounterState) count : CounterState = { state with count = count;}
+
+
+    // Neat method of finding the TryParse method for any type that supports it.
+    // See https://stackoverflow.com/a/33161245/158285
+    let inline tryParseWithDefault (defaultVal:'a) text : ^a when ^a : (static member TryParse : string * ^a byref -> bool) = 
+        let r = ref defaultVal
+        if (^a : (static member TryParse: string * ^a byref -> bool) (text, &r.contents)) 
+        then !r 
+        else defaultVal
+
+    module Binder =
+        let inline oneWay defaultValue update dispatch (sender:obj) _ =
+            let value = 
+                match sender with 
+                | :? Avalonia.Controls.TextBox as textBox -> tryParseWithDefault defaultValue (textBox.Text) 
+                | _ -> defaultValue
+            dispatch (fun state -> update state value ) 
+
+        let inline command update dispatch (sender:obj) _ =
+            dispatch (fun state -> update state)
+
     let view (state: CounterState) (dispatch): View =
         Views.dockpanel [
             Attrs.children [
                 Views.button [
                     Attrs.dockPanel_dock Dock.Bottom
-                    Attrs.onClick (fun sender args -> dispatch Decrement)
+                    Attrs.onClick (Binder.command increment dispatch)
                     Attrs.content "-"
                 ]
                 Views.button [
                     Attrs.dockPanel_dock Dock.Bottom
-                    Attrs.onClick (fun sender args -> dispatch Increment)
+                    Attrs.onClick (Binder.command decrement dispatch)
                     Attrs.content "+"
                 ]
-                Views.textBlock [
+                Views.textBox[
                     Attrs.dockPanel_dock Dock.Top
                     Attrs.fontSize 48.0
                     Attrs.verticalAlignment VerticalAlignment.Center
-                    Attrs.horizontalAlignment HorizontalAlignment.Center
+                    Attrs.horizontalAlignment HorizontalAlignment.Stretch
                     Attrs.text (string state.count)
+                    Attrs.onKeyUp (Binder.oneWay 0 set_count dispatch )
                 ]
             ]
         ]       
